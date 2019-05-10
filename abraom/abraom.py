7ca0f95cabf0f785494eb32f822c1736e5bc1ca8
Summer K. Rankin <1fmusic@users.noreply.github.com>
2019-05-10 15:27:08 -0400

    Add files via upload
    latest working versions of abraom

--- abraom/abraom.py
+++ abraom/abraom.py
@@ -1,6 +1,8 @@
 import sys
 from cravat import BaseAnnotator
 from cravat import InvalidData
+from pyliftover import LiftOver
+
 import sqlite3
 import os
 
@@ -14,9 +16,7 @@ class CravatAnnotator(BaseAnnotator):
         sqlite3.Connection object is stored as self.dbconn, and the
         sqlite3.Cursor object is stored as self.cursor.
         """
-        # Verify the connection and cursor exist.
-        assert isinstance(self.dbconn, sqlite3.Connection)
-        assert isinstance(self.cursor, sqlite3.Cursor)
+        pass
 
     def annotate(self, input_data, secondary_data=None):
         """
@@ -29,7 +29,7 @@ class CravatAnnotator(BaseAnnotator):
             ('uid', 'chrom', 'pos', 'ref_base', 'alt_base')
 
         keys in the annotator database (one flat table):
-        CHR  POS  ID   REF  ALT    A
+        CHR  Start  ID   REF  ALT    Frequencies
 
         should return a dictionary with keys matching the column names
         defined in example_annotator.yml. Extra column names will be ignored,
@@ -43,20 +43,29 @@ class CravatAnnotator(BaseAnnotator):
         input_ref = input_data['ref_base']
         input_alt = input_data['alt_base']
 
-        sql_q = 'SELECT  Frequencies FROM abraom WHERE CHR={} AND Start={} AND REF={} and ALT={};'.format(input_chrom, input_pos, input_ref, input_alt)
+
+        sql_q = 'SELECT  Frequencies FROM abraom WHERE CHR="%s" AND Start=%s AND REF="%s" and ALT="%s";' \
+                %(input_chrom, input_pos, input_ref, input_alt)
+
 
         self.cursor.execute(sql_q)
-        sql_q_result = self.cursor.fetchall()
+        sql_q_result = self.cursor.fetchone()
+
 
         allele_freq = ''
 
 
-
         if sql_q_result:
             allele_freq += sql_q_result[0]
+            #print(allele_freq)
             out['allele_freq'] = allele_freq
 
-            return out
+
+        else :
+            out['allele_freq'] = 9999.99
+
+
+        return out
 
 
     def cleanup(self):
